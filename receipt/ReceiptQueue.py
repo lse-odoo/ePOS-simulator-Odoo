64bc2a6a9c9479a708bb9275e38f1c4ad9a3c976
Loan (LSE) <lse@odoo.com>
2024-05-08 22:13:24 +0200

    [FIX] adapt to python3.8 (minimal python version)

--- receipt/ReceiptQueue.py
+++ receipt/ReceiptQueue.py
@@ -1,5 +1,6 @@
 
 from collections import deque
+from typing import List
 
 from receipt.ImageInfo import ImageInfo
 
@@ -8,5 +9,5 @@ _receipt_queue = deque(maxlen=50)
 def add_receipt_to_queue(image_info: ImageInfo) -> None:
     _receipt_queue.append(image_info)
 
-def get_receipts() -> list[ImageInfo]:
+def get_receipts() -> List[ImageInfo]:
     return list(_receipt_queue)[::-1]
